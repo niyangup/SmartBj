52e5e6b0a831dfd7523a61bb1576d67803a66472
niyangup <niyangup@163.com>
2019-06-05 07:53:06 +0800

    finish

--- gen/com/niyang/zhbj/R.java
+++ gen/com/niyang/zhbj/R.java
@@ -543,14 +543,14 @@ or to a theme attribute in the form "<code>?[<i>package</i>:][<i>type</i>:]<i>na
         public static final int fullscreen=0x7f090008;
         public static final int ib_next=0x7f090020;
         public static final int indicator=0x7f09001c;
-        public static final int iv_arrow=0x7f090022;
+        public static final int iv_arrow=0x7f090023;
         public static final int iv_icon=0x7f09001e;
         public static final int left=0x7f090006;
         public static final int ll_point=0x7f09000c;
         public static final int lv_list=0x7f090018;
         public static final int margin=0x7f090009;
         public static final int none=0x7f090001;
-        public static final int pb_loading=0x7f090023;
+        public static final int pb_loading=0x7f090022;
         public static final int rb_gov=0x7f090016;
         public static final int rb_home=0x7f090013;
         public static final int rb_news=0x7f090014;
@@ -595,11 +595,12 @@ or to a theme attribute in the form "<code>?[<i>package</i>:][<i>type</i>:]<i>na
         public static final int list_item_news=0x7f030009;
         public static final int pager_news_menu_detail=0x7f03000a;
         public static final int pager_tab_detail=0x7f03000b;
-        public static final int pull_to_refresh=0x7f03000c;
-        public static final int sliding_left_menu=0x7f03000d;
-        public static final int sliding_right_menu=0x7f03000e;
-        public static final int slidingmenumain=0x7f03000f;
-        public static final int title_bar=0x7f030010;
+        public static final int pull_to_refresh_foot=0x7f03000c;
+        public static final int pull_to_refresh_header=0x7f03000d;
+        public static final int sliding_left_menu=0x7f03000e;
+        public static final int sliding_right_menu=0x7f03000f;
+        public static final int slidingmenumain=0x7f030010;
+        public static final int title_bar=0x7f030011;
     }
     public static final class menu {
         public static final int main=0x7f0b0000;
